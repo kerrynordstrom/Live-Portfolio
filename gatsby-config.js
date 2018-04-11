1eeb50c887f5ad489d122833b619b5a3fd2a643f
Kerry Nordstrom <kerry.a.nordstrom@gmail.com>
2018-04-11 12:26:23 -0500

    uuu

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
     author: "Kerry Nordstrom",
     description: "Kerry Nordstrom Portfolio"
   },
-  pathPrefix: "/live-portfolio",
+
   plugins: [
     {
       resolve: `gatsby-plugin-typography`,
